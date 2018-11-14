854422c9bc3c8674518ca3b4daa507670649c675
github-ek <esb.ega.kat@gmail.com>
2018-11-14 08:43:47 -0500

    Separacion de proyecto

--- egakat-e-connect-config-client/src/main/java/com/egakat/econnect/config/client/components/ConnectConfigRestClient.java
+++ egakat-e-connect-config-client/src/main/java/com/egakat/econnect/config/client/components/ConnectConfigRestClient.java
@@ -1,10 +1,19 @@
 package com.egakat.econnect.config.client.components;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.egakat.core.web.client.components.AbstractRestClientImpl;
 
 @Component
 public class ConnectConfigRestClient extends AbstractRestClientImpl {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Override
+	public RestTemplate getRestTemplate() {
+		return restTemplate;
+	}
 }
